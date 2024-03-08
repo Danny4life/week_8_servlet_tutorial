6d7e2f3aa7b0c45aedf332e38d4bead546063ded
Danny4life <osiki.segun@gmail.com>
2024-03-08 04:20:36 +0100

    commit files

---
+++ src/main/java/com/osiki/week8demo/MyServlet.java
@@ -0,0 +1,22 @@
+package com.osiki.week8demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "myServlet", value = "/my-servlet")
+public class MyServlet extends HttpServlet {
+
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("A post request has been made");
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("A get request has been made");
+    }
+}
